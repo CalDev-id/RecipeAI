93e5407136298f5672c0ca811897767f1369dcae
Heical Chandra <haical31@gmail.com>
2024-05-29 14:11:15 +0700

    Initial commit

---
+++ RecipeAI/RecipeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAIApp.swift
+//  RecipeAI
+//
+//  Created by Heical Chandra on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
